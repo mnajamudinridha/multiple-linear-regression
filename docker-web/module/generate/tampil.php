d826886fad3068e37bfa07ab523cc574c0c9e8d8
najcardboyz <najcardboyz.03@gmail.com>
2020-08-16 13:56:16 +0800

    add jupyter

--- docker-web/module/generate/tampil.php
+++ docker-web/module/generate/tampil.php
@@ -16,43 +16,43 @@ if (empty($_SESSION['login']) || $_SESSION['login'] != 'admin') {
     include 'vendor/autoload.php';
     include 'fungsi/koneksi.php';
     
-    // /* Generate Table dan Field */
-    // mysqli_query($con, "DROP TABLE x_generate");
-    // $kuisioner = mysqli_query($con, "SELECT * FROM kuisioner WHERE jenis='1'");
-    // $totaldata = mysqli_num_rows($kuisioner);
-    // $createtbl = "CREATE TABLE x_generate (kode VARCHAR(200) PRIMARY KEY, ";
-    // $no = 1;
-    // while ($kuis = mysqli_fetch_array($kuisioner)) {
-    //     if($no == ($totaldata)){
-    //         $createtbl .= "$kuis[kode] INT(11) NOT NULL ";
-    //     }else{
-    //         $createtbl .= "$kuis[kode] INT(11) NOT NULL, ";
-    //     }
-    //    $no++;
-    // }
-    // $createtbl .= " )";
-    // mysqli_query($con, $createtbl);
-    // /* Generate Table dan Field */
+    /* Generate Table dan Field */
+    mysqli_query($con, "DROP TABLE x_generate");
+    $kuisioner = mysqli_query($con, "SELECT * FROM kuisioner WHERE jenis='1'");
+    $totaldata = mysqli_num_rows($kuisioner);
+    $createtbl = "CREATE TABLE x_generate (kode VARCHAR(200) PRIMARY KEY, ";
+    $no = 1;
+    while ($kuis = mysqli_fetch_array($kuisioner)) {
+        if($no == ($totaldata)){
+            $createtbl .= "$kuis[kode] INT(11) NOT NULL ";
+        }else{
+            $createtbl .= "$kuis[kode] INT(11) NOT NULL, ";
+        }
+       $no++;
+    }
+    $createtbl .= " )";
+    mysqli_query($con, $createtbl);
+    /* Generate Table dan Field */
 
-    // /* Isi Data Field */
-    // $dataset = mysqli_query($con, "SELECT * FROM dataset GROUP BY kode");
-    // while ($a = mysqli_fetch_array($dataset)) {
-    //     $kuisioner = mysqli_query($con, "SELECT * FROM kuisioner WHERE jenis='1'");
-    //     $totaldata = mysqli_num_rows($kuisioner);
-    //     $insert = "INSERT INTO x_generate VALUES ('".$a['kode']."',";
-    //     $no = 1;
-    //     while ($kuis = mysqli_fetch_array($kuisioner)) {
-    //         $val = mysqli_fetch_array(mysqli_query($con, "SELECT * FROM dataset WHERE kode='$a[kode]' AND kuisioner='$kuis[kode]'"));
-    //         if($no == ($totaldata)){
-    //             $insert .= "'".$val['value']."'";
-    //         }else{
-    //             $insert .= "'".$val['value']."',";
-    //         }
-    //         $no++;
-    //     }
-    //     $insert .= ");";
-    //     mysqli_query($con, $insert);
-    // }
+    /* Isi Data Field */
+    $dataset = mysqli_query($con, "SELECT * FROM dataset GROUP BY kode");
+    while ($a = mysqli_fetch_array($dataset)) {
+        $kuisioner = mysqli_query($con, "SELECT * FROM kuisioner WHERE jenis='1'");
+        $totaldata = mysqli_num_rows($kuisioner);
+        $insert = "INSERT INTO x_generate VALUES ('".$a['kode']."',";
+        $no = 1;
+        while ($kuis = mysqli_fetch_array($kuisioner)) {
+            $val = mysqli_fetch_array(mysqli_query($con, "SELECT * FROM dataset WHERE kode='$a[kode]' AND kuisioner='$kuis[kode]'"));
+            if($no == ($totaldata)){
+                $insert .= "'".$val['value']."'";
+            }else{
+                $insert .= "'".$val['value']."',";
+            }
+            $no++;
+        }
+        $insert .= ");";
+        mysqli_query($con, $insert);
+    }
     /* Isi Data Field */
     $command = escapeshellcmd("");
     $output = shell_exec("python3 generate.py");
